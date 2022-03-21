8852451c802de043da2bd0edf346ad07417e523c
dev4589 <86591336+dev4589@users.noreply.github.com>
2022-03-21 10:06:10 +0530

    added files from original project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:erp_sem4/screens/login.dart';
+import 'package:erp_sem4/utility/demo.dart';
 import 'package:erp_sem4/utility/login_module.dart';
 import 'package:flutter/material.dart';
 import 'screens/slash_screen.dart';
@@ -8,17 +10,14 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: FirebaseOptions(
+      options: const FirebaseOptions(
           apiKey: "AIzaSyCl2rclQDlZ7L8rIZxKUMI3qmUV7MB2rLk",
           authDomain: "erp-sem4.firebaseapp.com",
           projectId: "erp-sem4",
           storageBucket: "erp-sem4.appspot.com",
           messagingSenderId: "332909613315",
           appId: "1:332909613315:web:be23cdb27b5b0675b69936",
-
-          measurementId: "G-XCEHEC2TW3"
-
-      ));
+          measurementId: "G-XCEHEC2TW3"));
   runApp(MyApp());
 }
 
@@ -27,29 +26,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color kPrimaryColor = Colors.blue;
     return MaterialApp(
-        builder: (context, widget) => ResponsiveWrapper.builder(
-          BouncingScrollWrapper.builder(context, widget!),
-          // maxWidth: 1200,
-          // minWidth: 480,
-          defaultScale: true,
-          breakpoints: [
-            ResponsiveBreakpoint.resize(480, name: MOBILE),
-            ResponsiveBreakpoint.resize(800, name: TABLET),
-            ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-            ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-            ResponsiveBreakpoint.resize(2460, name: '4K'),
-          ],
-          background: Container(color: Colors.black),
-        ),
-        title: 'Login',
-        theme: ThemeData(
-          primaryColor: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          textTheme: GoogleFonts.secularOneTextTheme(),
-        ),
-        debugShowCheckedModeBanner: false,
-        home: LogIn(),
-      );
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, widget!),
+        // maxWidth: 1200,
+        // minWidth: 480,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(480, name: MOBILE),
+          ResponsiveBreakpoint.resize(800, name: TABLET),
+          ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          ResponsiveBreakpoint.resize(2460, name: '4K'),
+        ],
+        background: Container(color: Colors.black),
+      ),
+      title: 'Login',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.secularOneTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
   }
 }
