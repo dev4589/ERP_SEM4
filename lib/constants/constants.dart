5698ee54adfe96b776be4cbd50a74a9cb6694c7f
parth1215 <86591802+parth1215@users.noreply.github.com>
2022-04-06 11:36:00 +0530

    done

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 //  Color for 1st half of login Page and primary theme color
 
 Color kPrimaryColor = Color.fromRGBO(55, 105, 151, 1);
+Color bColor= Colors.yellowAccent.shade100;
 
 TextStyle gridviewtext = new TextStyle(fontSize: 16);
